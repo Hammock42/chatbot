21245b754d389e07253221b86b13ded71b6cb9fd
Hamad Hamdi <Hammock42@protonmail.com>
2024-08-25 06:53:15 +0200

    Implement chatbot api response

--- chatbot_project/chatbot_project/views.py
+++ chatbot_project/chatbot_project/views.py
@@ -1,5 +1,16 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+import json
 
+
+
+@csrf_exempt
 def api(request):
-    test_response = {"message": "Hello, world!"}
-    return JsonResponse(test_response)
+    
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_msg = data['message']
+        ## ai_msg = get_ai_response(user_msg)
+        ai_msg = "Hello, I am a chatbot. How can I help you?"
+        resp = {'message': ai_msg}
+    return JsonResponse(resp) 
